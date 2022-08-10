2298ba9cf9ae2e5036e23d09ddbe8d0318038faa
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-08-09 22:42:37 -0300

    chore: initial project config, create-next-app cleanup, install necessary libs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Catálogo Pokemon TCG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>Home</p>
+    </>
+  )
+}
+
+export default Home
